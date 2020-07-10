fcd102ef02c6bd25b8edb44c3b2f921cc401414d
Pietro.Cosentino <pietrocosentino88@gmail.com>
2020-07-10 12:38:20 -0300

    second commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo1.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numeroDigitado;
+
+            Console.Write("Digite um número: ");
+
+            numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(numeroDigitado.ToString());
+
+            if (numeroDigitado % 2 == 0)
+            {
+                Console.Write("Par");
+
+            }else{
+
+                Console.Write("Impar");
+            }
+                          
+            Console.ReadKey();
+
+        }
+    }
+}
